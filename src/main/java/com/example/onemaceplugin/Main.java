9c5ec7384cced7c58961258048fb8143d270e810
nggouravyt <onlytalk098@gmail.com>
2025-03-09 16:03:08 +0530

    Update Main.java

--- src/main/java/com/example/onemaceplugin/Main.java
+++ src/main/java/com/example/onemaceplugin/Main.java
@@ -1,1 +1,54 @@
 
+package com.example.onemaceplugin;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.CraftItemEvent;
+import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.event.player.PlayerDeathEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin implements Listener {
+    private boolean maceExists = false;
+
+    @Override
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onCraft(CraftItemEvent event) {
+        if (event.getRecipe().getResult().getType() == Material.MACE) {
+            if (maceExists) {
+                event.setCancelled(true);
+            } else {
+                maceExists = true;
+            }
+        } else {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onDrop(PlayerDropItemEvent event) {
+        if (event.getItemDrop().getItemStack().getType() == Material.MACE) {
+            Bukkit.getScheduler().runTaskLater(this, () -> {
+                if (event.getItemDrop().isDead()) {
+                    maceExists = false;
+                }
+            }, 6000);
+        }
+    }
+
+    @EventHandler
+    public void onDeath(PlayerDeathEvent event) {
+        for (ItemStack drop : event.getDrops()) {
+            if (drop.getType() == Material.MACE) {
+                return;
+            }
+        }
+    }
+}
